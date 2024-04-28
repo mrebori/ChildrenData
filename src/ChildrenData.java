2a8f4fd3cbd4f8f2fc51dacb824d538dc5bff11e
Mrebori <mary.rebori@icloud.com>
2024-04-28 19:45:29 -0400

    Initial commit

---
+++ src/ChildrenData.java
@@ -0,0 +1,21 @@
+public class ChildrenData {
+    public static void main(String[] args) {
+        System.out.println("Children's Names and Ages:");
+
+        String kid1Name = "Michael Scott";
+        String kid2Name = "Dwight Schrute";
+        String kid3Name = "Jim Harper";
+
+        int kid1Age = 3;
+        int kid2Age = 5;
+        int kid3Age = 8;
+
+        char kid1Jersey = 's';
+        char kid2Jersey = 'm';
+        char kid3Jersey = 'l';
+
+        System.out.println(kid1Name + " is " + kid1Age + " years old. Jersey size: " + kid1Jersey);
+        System.out.println(kid2Name + " is " + kid2Age + " years old. Jersey size: " + kid2Jersey);
+        System.out.println(kid3Name + " is " + kid3Age + " years old. Jersey size: " + kid3Jersey);
+    }
+}
---
+++ src/FirstProject.java
@@ -0,0 +1,14 @@
+public class FirstProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String myVariable = "example value";
+        System.out.println("The value of my variable is: " + myVariable);
+        Integer kidsAge = 4;
+        System.out.println("Max's age is: " + kidsAge);
+        int number = 11;
+        System.out.println("Tyler's age is: " + number);
+        byte kid3 = 5;
+        System.out.println("Kid3's age is: " + kid3);
+    }
+}
